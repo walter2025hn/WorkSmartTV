3abeab20b87d5e932d35c6aa221b15917d0a08e3
walter2025hn <saidantunez4@gmail.com>
2026-08-25 18:20:19 -0600

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.exoplayer:exoplayer:2.19.1")
+    implementation("com.google.android.exoplayer:exoplayer-hls:2.19.1")
+    // Para Android TV
+    implementation("androidx.leanback:leanback:1.0.0")
+}
