6b053b8cc1302f3d51d488f32ad408dbf49b874b
walter2025hn <saidantunez4@gmail.com>
2026-08-25 19:05:27 -0600

    Refactor pluginManagement and update repositories mode

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,18 @@
 pluginManagement {
- pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    // Cambiamos a PREFER_SETTINGS para que use los repositorios definidos aquí
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
     }
 }
+
 rootProject.name = "WorkSmartTV"
 include(":app")
